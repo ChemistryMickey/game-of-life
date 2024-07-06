7f2b5bcd0becc50cfefc7aca30455be052b10851
Michael Ryan T Da Silva <michele.tdasilva@gmail.com>
2024-07-06 16:12:41 -0700

    Added acorn board. Separated out game logic into subfunctions

--- rust/src/game_rules.rs
+++ rust/src/game_rules.rs
@@ -1,7 +1,27 @@
 use crate::board::Board;
 
 pub fn apply_game_of_life_rules(board: &mut Board) -> () {
+    let (life_board, death_board) = get_life_death_boards(board);
+    update_board(board, life_board, death_board);
+}
+
+fn update_board(board: &mut Board, life_board: Board, death_board: Board) -> () {
     let n_side = board.cells.len();
+    for i in 0..n_side {
+        for j in 0..n_side {
+            if death_board.cells[i][j] {
+                board.cells[i][j] = false;
+            }
+            if life_board.cells[i][j] {
+                board.cells[i][j] = true;
+            }
+        }
+    }
+}
+
+fn get_life_death_boards(board: &mut Board) -> (Board, Board) {
+    let n_side = board.cells.len();
+
     let mut life_board = Board::new(n_side);
     let mut death_board = Board::new(n_side);
 
@@ -18,14 +38,5 @@ pub fn apply_game_of_life_rules(board: &mut Board) -> () {
         }
     }
 
-    for i in 0..n_side {
-        for j in 0..n_side {
-            if death_board.cells[i][j] {
-                board.cells[i][j] = false;
-            }
-            if life_board.cells[i][j] {
-                board.cells[i][j] = true;
-            }
-        }
-    }
+    return (life_board, death_board);
 }
